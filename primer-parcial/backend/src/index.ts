4cc9c95b2ba6ae32924bb5f4370b914dad49d3d2
Lucas Julian Guerci <lucas.guerci@gmail.com>
2023-09-21 13:25:44 -0300

    logged gcp creds

--- primer-parcial/backend/src/index.ts
+++ primer-parcial/backend/src/index.ts
@@ -2,11 +2,17 @@ import express from "express";
 import bodyParser from "body-parser";
 import helmet from "helmet";
 import routes from "./routes";
-import cors from 'cors'
+import cors from "cors";
 
-const corsEnabledDomains = process.env.CORS_ENABLED_ORIGINS?.split(',') ?? ['http://127.0.0.1:5173','http://localhost:5173']
+const gcp = require("../credentials/gcp.json");
+console.log({ gcp });
 
-console.log({corsEnabledDomains})
+const corsEnabledDomains = process.env.CORS_ENABLED_ORIGINS?.split(",") ?? [
+  "http://127.0.0.1:5173",
+  "http://localhost:5173",
+];
+
+console.log({ corsEnabledDomains });
 
 const PORT = 5000;
 
@@ -15,12 +21,14 @@ const app = express();
 app.use(bodyParser.json());
 app.use(helmet());
 
-app.use(cors({
-  origin: corsEnabledDomains,
-  credentials: true,
-}))
+app.use(
+  cors({
+    origin: corsEnabledDomains,
+    credentials: true,
+  })
+);
 
-app.use(routes)
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`App listening on http://localhost:${PORT}/`);
