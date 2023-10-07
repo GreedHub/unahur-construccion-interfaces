e31935e6b7603542818b55cf73baa5e31929e309
Lucas Julian Guerci <lucas.guerci@gmail.com>
2023-10-07 12:02:59 -0300

    added purchased msg

--- tp2/src/components/product-list/product-item/index.tsx
+++ tp2/src/components/product-list/product-item/index.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, useContext } from 'react';
+import { ReactElement, useContext, useState } from 'react';
 import './styles.scss'
 import Product from '../../../types/product';
 import CartContext from '../../../context/cart'
@@ -11,8 +11,19 @@ export default function ProductListItem(props:ProductListItemProps):ReactElement
     const { image, name, price, description } = props.product
     const { addProduct } = useContext(CartContext)
 
+    const BUY_MSG = 'Agregar al carrito'
+    const PURCHASED_MSG = 'Agregado al carrito!'
+
+    const [ buyBtn, setBuyBtn ] = useState<string>(BUY_MSG)
+
     const onPurchase = ()=>{
         addProduct(props.product)
+
+        setBuyBtn(()=>PURCHASED_MSG)
+
+        setTimeout(() => {
+            setBuyBtn(()=>BUY_MSG)
+        }, 1000);
     }
 
     return (
@@ -28,7 +39,7 @@ export default function ProductListItem(props:ProductListItemProps):ReactElement
             <div className='product-list__item-description'>
                 {description}
             </div>
-            <button onClick={onPurchase}>Comprar</button>
+            <button onClick={onPurchase}>{buyBtn}</button>
         </li>
     )
 }
