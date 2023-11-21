f650ab55f71fa4947a22c5e98f81d86621575bf2
Lucas Julian Guerci <lucas.guerci@gmail.com>
2023-11-21 17:43:48 -0300

    improved styles

--- segundo-parcial/src/components/message/content.tsx
+++ segundo-parcial/src/components/message/content.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import "./styles.scss";
 
 type ForumMessageContentProps = {
@@ -7,9 +8,29 @@ type ForumMessageContentProps = {
 export default function ForumMessageContent(props: ForumMessageContentProps) {
   const { content } = props;
 
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  const setMaxWidth = () => {
+    const content = contentRef.current;
+    if (!content) return;
+
+    const parent = content.parentElement as HTMLDivElement;
+    if (!parent) return;
+
+    const parentWidth = parent.clientWidth;
+
+    content.style.maxWidth = `${parentWidth - 40}px`;
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", setMaxWidth);
+    setMaxWidth();
+  });
+
   return (
     <div
       className="forum-message__content"
+      ref={contentRef}
       dangerouslySetInnerHTML={{ __html: content }}
     ></div>
   );
--- segundo-parcial/src/components/nav-desktop/index.tsx
+++ segundo-parcial/src/components/nav-desktop/index.tsx
@@ -11,20 +11,22 @@ export default function MobileNav(props: MobileNavProps) {
   const { user } = props;
 
   return (
-    <nav className="mobile-nav mobile">
-      <Link to="/">
-        <Home />
-      </Link>
-      <Link to="/search">
-        <Search />
-      </Link>
-      <Link to="/favorites">
-        <Star />
-      </Link>
-      <Link to="/profile">
-        {user && <img src={user.picture} alt="user_picture" />}
-        {!user && <AccountCircle />}
-      </Link>
-    </nav>
+    <header>
+      <nav className="mobile-nav mobile">
+        <Link to="/">
+          <Home />
+        </Link>
+        <Link to="/search">
+          <Search />
+        </Link>
+        <Link to="/favorites">
+          <Star />
+        </Link>
+        <Link to="/profile">
+          {user && <img src={user.picture} alt="user_picture" />}
+          {!user && <AccountCircle />}
+        </Link>
+      </nav>
+    </header>
   );
 }
